24f2519a136486be5df242602aec3e84a92cec27
ZacharyJBartlett <Zachary.jamesbartlett@gmail.com>
2017-05-10 16:52:10 -0400

    Update bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,7 +6,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Cadillac1!",
+    password: "",
     database: "bamazon_db"
 });
 connection.connect(function(err) {
